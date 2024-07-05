2677d09ff8bfcad5941221af182938a3b191648a
misha <shma2001@gmail.com>
2024-07-05 17:36:00 +0200

    add tests for the encrypting and signing

--- bash/encrypt.sh
+++ bash/encrypt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source ./env.sh
-cat "text-to-encrypt" | openssl pkeyutl -encrypt -inkey ${KEYS_DIR}/public_key.pem -pubin -out encrypted_message.bin
+cat "text-to-encrypt" | openssl pkeyutl -encrypt -inkey ${PARENT}/${KEYS_DIR}/public_key.pem -pubin -out encrypted_message.bin
